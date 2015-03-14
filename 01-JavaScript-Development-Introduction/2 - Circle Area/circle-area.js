60e3bf6a2d9216f2bf49e06b30a6d4b44ddc226f
Svetlozar Kirkov <svetlozark@gmail.com>
2015-03-14 20:23:01 +0200

    pre-final

--- 01-JavaScript-Development-Introduction/2 - Circle Area/circle-area.js
+++ 01-JavaScript-Development-Introduction/2 - Circle Area/circle-area.js
@@ -1,1 +1,18 @@
+function write(input, result) {
+			var paragraph = document.createElement("P");
+			var text = document.createTextNode("r = " + input + "; area = " + result);
+			paragraph.appendChild(text);
+			document.getElementById("content").appendChild(paragraph);
+}
 
+function calcArea(r) {
+	var result = Math.PI*r*r;
+	console.log(result);
+	write(r,result);
+}
+
+function writeOnHtml() {
+	calcArea(7);
+	calcArea(1.5);
+	calcArea(20);
+}
