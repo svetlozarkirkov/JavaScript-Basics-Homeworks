3b6e01c44ab8565e336b5cf7729c341fb3329355
Svetlozar Kirkov <svetlozark@gmail.com>
2015-03-16 17:09:15 +0200

    updated

--- 02-JavaScript-Syntax/1 - Quadratic Equation/quadratic-equation.js
+++ 02-JavaScript-Syntax/1 - Quadratic Equation/quadratic-equation.js
@@ -1,3 +1,28 @@
-/**
- * Created by John on 15.3.2015 г..
- */
+"use strict";
+function solveQuadratic(a, b, c) {
+    var sqrtpart = b * b - 4 * a * c;
+    var x, x1, x2;
+    if (sqrtpart > 0){
+        x1 = (-b - Math.sqrt(sqrtpart)) / (2 * a);
+        x2 = (-b + Math.sqrt(sqrtpart)) / (2 * a);
+        console.log("x1 = " + x1 + "\nx2 = " + x2);
+    }
+    else if (sqrtpart < 0) {
+        console.log("No real roots");
+    }
+    else {
+        x = (-b + Math.sqrt(sqrtpart)) / (2 * a);
+        console.log("x = " + x);
+    }
+}
+var promptA = window.prompt("Enter number for \"a\"");
+var numA = parseFloat(promptA);
+console.log("a = " + numA);
+var promptB = window.prompt("Enter number for \"b\"");
+var numB = parseFloat(promptB);
+console.log("b = " + numB);
+var promptC = window.prompt("Enter number for \"c\"");
+var numC = parseFloat(promptC);
+console.log("c = " + numC);
+
+solveQuadratic(numA, numB, numC);
---
+++ 02-JavaScript-Syntax/2 - Calculate Knots/calculate-knots.js
@@ -0,0 +1,8 @@
+"use strict";
+function convertToKnots(kmh) {
+    var result = kmh * 0.539956803;
+    result = result.toFixed(2);
+    console.log("knots = " + result);
+}
+
+/* run this function in browser */
