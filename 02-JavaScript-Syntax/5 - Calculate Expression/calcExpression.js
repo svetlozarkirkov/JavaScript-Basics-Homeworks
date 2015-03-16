ae3be70a31924410a3013cf108750a3d3786d9ff
Svetlozar Kirkov <svetlozark@gmail.com>
2015-03-16 19:13:20 +0200

    final beta

--- 02-JavaScript-Syntax/5 - Calculate Expression/calcExpression.js
+++ 02-JavaScript-Syntax/5 - Calculate Expression/calcExpression.js
@@ -1,1 +1,8 @@
-
+"use strict";
+function evaluateAndPrint() {
+    var input = document.getElementById('inputBox').value;
+    console.log("input = " + input);
+    var result = eval(input);
+    document.getElementById('resultBox').innerHTML = String(result);
+    console.log("result = " + result);
+}
