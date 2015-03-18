fab354b4e7c2c17017ef748b4c7731a0fe8ca7c1
Svetlozar Kirkov <svetlozark@gmail.com>
2015-03-18 22:52:53 +0200

    optimizations

--- 03-JavaScript-Loops-Arrays-Strings/1 - Array Manipulator/array-manipulator.js
+++ 03-JavaScript-Loops-Arrays-Strings/1 - Array Manipulator/array-manipulator.js
@@ -1,8 +1,10 @@
 "use strict";
 function manipulate(arr) {
+    
     function isNum(num) {
         return !isNaN(num);
     }
+
     function mostOccurring(arr) {
         var mf = 1;
         var m = 0;
@@ -14,25 +16,28 @@ function manipulate(arr) {
                 if (arr[i] === arr[j]) {
                     m++;
                 }
-                if (mf<m)
+                if (mf < m)
                 {
                     mf = m;
                     item = arr[i];
                 }
             }
-            m=0;
+            m = 0;
         }
         return item;
     }
+
     var filtered = [];
+    
     for (var i in arr){
         if(isNum(arr[i])){
             filtered.push(arr[i]);
         }
     }
+    
     filtered.sort(function(a, b){return b-a;});
-    var min = Math.min.apply(null, filtered),
-        max = Math.max.apply(null, filtered);
+    var min = Math.min.apply(null, filtered);
+    var max = Math.max.apply(null, filtered);
     var mostFreq = mostOccurring(filtered);
     console.log('Min number: ' + min);
     console.log('Max number: ' + max);
---
+++ 03-JavaScript-Loops-Arrays-Strings/5 - Group People/group-people.js
@@ -0,0 +1,9 @@
+"use strict";
+function Person(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+}
+function group(persons, prop) {
+
+}
